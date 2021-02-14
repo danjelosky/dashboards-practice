112a00380e9d7c056e38b256fc32eb4738051b89
danjelosky <55547105+danjelosky@users.noreply.github.com>
2021-02-13 18:18:51 -0600

    commit

--- FootballCard.js
+++ FootballCard.js
@@ -3,3 +3,12 @@ $(document).ready(function(){
     $("#liverpoolCard").hide();
   });
 });
+
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=2ab0bce971734ebf99887918fd4c419b';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
